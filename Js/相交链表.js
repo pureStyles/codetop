52a58129ebc84aaf392b0fd3135ce7d446fc5c0f
pureStyles <siyuan.wen@outlook.com>
2024-11-23 11:15:46 +0800

    feat(相交链表): 我走你走过的路，只为了和你相拥

--- Js/相交链表.js
+++ Js/相交链表.js
@@ -16,3 +16,15 @@ var getIntersectionNode = function(headA, headB) {
     }
     return null;
 }
+
+// 优化内存空间算法
+var getIntersectionNode = function(headA, headB) {
+    // 判断走过的路径一样不一样
+    // 如果一样的话就结束了，不一样的话还要判断是否为Null;
+    let A = headA, B = headB;
+    while(A !== B) {
+        A = A !== null ? A.next : headB;
+        B = B !== null ? B.next : headA;
+    }
+    return A;
+}
